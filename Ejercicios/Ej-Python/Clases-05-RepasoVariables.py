48dcde2fa63506d57e5fcc0148bc9846edda1660
sofiprodanov <sofiabprodanov@gmail.com>
2025-05-16 11:53:42 -0300

    Ej de la teoria

---
+++ Ejercicios/Ej-Python/Clases-05-RepasoVariables.py
@@ -0,0 +1,51 @@
+#Mirar archivo "Clases-05-Repaso.psc" 
+
+#TEMA VARIABLES
+saludo = "¡Hola mundo!"
+print (saludo)
+
+
+#TEMA TIPOS DE DATOS
+numero_entero = int
+numero_decimal = float
+candena_de_caracteres = str
+logico_booleano = bool
+
+
+#TEMA ESTRUCTURAS DE CONTROL: CONDICIONALES
+variable1 = 10
+variable2 = 20
+
+if variable1 > variable2:
+    print("La variable 1 es mayor.")
+elif variable1 < variable2:
+    print("La variable 2 es mayor.")
+else:
+    print("Las variables son iguales.")
+
+
+#TEMA ESTRUCTURAS DE CONTROL: BUCLE
+i = 0
+
+while i < 5:
+    i += 1
+    print(i)
+
+
+for i in range(1, 6):
+    print(i)
+
+
+#TIPOS DE DATOS
+lista = [1, 2, 3, "aca va un texto, True"]
+print(lista)
+
+tupla = (1, 2, 3, "aca va un texto, True")
+print(tupla)
+
+diccionario = {
+    "clave": "valor",
+    "numero": 10,
+    "bool": True
+}
+print(diccionario)
