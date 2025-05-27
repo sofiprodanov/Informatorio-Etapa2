281d6f9adbc7daae0415ac9a9b5da701b86e094f
sofiprodanov <sofiabprodanov@gmail.com>
2025-05-27 10:09:05 -0300

    Ej Clase

--- Ejercicios/Ej-Python/Clases/Clase 6.3-Colecciones-Diccionarios.py
+++ Ejercicios/Ej-Python/Clases/Clase 6.3-Colecciones-Diccionarios.py
@@ -8,3 +8,44 @@ estudiante = {
     "curso": "Matematica"
 }
 
+print(f"Conjunto diccionario: {estudiante}, {type(estudiante)}")
+print(f"Edad estudiante: {estudiante['edad']}") #acceder a valores
+#Las claves no se repiten, pero puede haber varios valores.
+
+estudiante["carrera"] = "Ing" #para agregar una clave-valor
+print(f"Carrera estudiante: {estudiante['Carrera']}")
+
+del estudiante["carrera"]#para eliminar un elemento/clave junto con su valor
+print(f"Estudiante: {estudiante}")
+
+print(f"Claves (keys) del diccionario: {estudiante.keys()}") #muestra las claves
+print(f"Valores (values) del diccionario: {estudiante.values()}") #muestra los valores
+
+
+estudiante_dos = {
+    "nombre": "Juan",
+    "edad": 23,
+    "curso": {
+        "nombre": "Programacion web",
+        "tags": ["PW", "Programacion", "Web", "Python", "Django"]
+    }
+}
+
+print(f"Estudiante dos: {estudiante_dos['curso']['nombre']}")
+print(f"Estudiante dos: {estudiante_dos['curso']['tags'][3]}")
+
+
+estudiante_dos["curso"]["tags"][5]["algo"] = 99
+print(f"Estudiante dos: {estudiante_dos['curso']['tags'][5]["algo"]}")
+
+
+
+#usando for
+calificaciones = {
+    "quimica": 10,
+    "fisica": 5,
+    "lengua": 7
+}
+
+for materia, nota in calificaciones.items():
+    print(materia, nota)
